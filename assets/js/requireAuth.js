ea799094c5c016a7bd4b2d13cbc0f476a3815d5f
yuzomesu <yuzuomeikuai@gmail.com>
2025-06-08 02:39:11 +0900

    Update requireAuth.js

--- assets/js/requireAuth.js
+++ assets/js/requireAuth.js
@@ -1,6 +1,6 @@
 import { supabase } from './supabase-init.js';
 export async function requireAuth(){
   const { data:{ user } } = await supabase.auth.getUser();
-  if(!user){ location.href = '/login.html'; throw new Error('redirect'); }
+  if(!user){ location.href = new URL('login.html', location.origin + location.pathname).href; throw new Error('redirect'); }
   return user;
 }
